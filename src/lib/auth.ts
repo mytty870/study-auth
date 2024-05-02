3b86878c4f3848879f2289192c393f20df403d3b
mytty870 <otani11ace@icloud.com>
2024-05-02 23:00:20 +0900

    first commit

---
+++ src/lib/auth.ts
@@ -0,0 +1,47 @@
+import { PrismaAdapter } from "@next-auth/prisma-adapter"
+import { NextAuthOptions } from "next-auth"
+import GoogleProvider from "next-auth/providers/google"
+import {prisma} from "@/lib/prisma"
+
+export const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(prisma),
+  session: {
+    strategy: "jwt",
+  },
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID || "",
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
+    })
+  ],
+  callbacks: {
+    async jwt({ token, user }) {
+      const dbUser = await prisma.user.findFirst({
+        where: { email: token.email}
+      })
+      if (!dbUser) {
+        if (user) {
+          token.id = user?.id
+        }
+        return token
+      }
+      return {
+        id: dbUser.id,
+        name: dbUser.name,
+        email: dbUser.email,
+        picture: dbUser.image,
+      }
+    },
+    async session({ session, token }) {
+      if (session.user) {
+          session.user.id = token.id as string;
+          session.user.name = token.name;
+          session.user.email = token.email;
+          session.user.image = token.picture;
+      }
+      return session
+    }
+  },
+
+
+}
