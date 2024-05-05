2dbaec9b009417c80fca6b243cd7ae346de3892d
mytty870 <otani11ace@icloud.com>
2024-05-05 18:03:33 +0900

    fix Button

--- src/app/components/Button.tsx
+++ src/app/components/Button.tsx
@@ -1,31 +1,67 @@
-import { forwardRef } from "react";
-import { GoogleIcon } from "./icons/GoogleIcon";
+import { cn } from "@/lib/utils";
+import { cva } from "class-variance-authority";
+import { ButtonHTMLAttributes, forwardRef } from "react";
 
-export type ButtonProps = {
-  children: React.ReactNode
-  startContent?: React.ReactElement
-  endContent?: React.ReactElement
-}
-
-export const Button = ({
-  children,
-  startContent,
-  endContent,
-}: ButtonProps) => {
+const ButtonIconWrapper = ({children}: {children: React.ReactNode}) => {
   return (
-    <button>
-      {startContent && (
-        <span className="inline-flex items-center">
-          {startContent}
-        </span>
-      )}
+    <span className="inline-flex items-center">
       {children}
-      {endContent && (
-        <span className="inline-flex items-center">
-          {endContent}
-        </span>
-      )}
-    </button>
+    </span>
   )
-
 }
+
+const buttonVariants = cva(
+  "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium cursor-pointer disabled:cursor-not-allowed disabled:opacity-50 focus:ring-4 gap-2",
+  {
+    variants: {
+      variant: {
+        default: "bg-blue-400 text-white hover:bg-blue-400/90 focus:ring-blue-300",
+        defaultOutline: "bg-[#FFCC33] text-white hover:bg-[#FFCC33]/90 focus:ring-[#FFCC33]/30 ",
+        primary: "",
+
+      },
+      size: {
+        default: "h-10 px-4 py-2"
+
+      },
+      fullWidth: {
+        true: "w-full"
+      }
+    },
+    defaultVariants: {
+      // variant: "default",
+      variant: "defaultOutline",
+      size: "default",
+      fullWidth: false
+    }
+  }
+)
+
+export interface ButtonProps
+  extends ButtonHTMLAttributes<HTMLButtonElement> {
+    startContent?: React.ReactElement
+    endContent?: React.ReactElement
+  }
+
+export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ className, children, startContent, endContent, ...props }: ButtonProps, ref) => {
+    return (
+      <button
+        ref={ref}
+        className={cn(buttonVariants({ className}))}
+        type="button"
+        {...props}
+      >
+        {startContent && (
+          <ButtonIconWrapper>{startContent}</ButtonIconWrapper>
+        )}
+        {children}
+        {endContent && (
+          <ButtonIconWrapper>{endContent}</ButtonIconWrapper>
+        )}
+      </button>
+    );
+  }
+);
+
+Button.displayName = "Button"
--- src/app/components/LoginButtonGroup.tsx
+++ src/app/components/LoginButtonGroup.tsx
@@ -1,24 +1,35 @@
 'use client'
 
-import { Button } from "@nextui-org/react"
 import { signIn } from "next-auth/react"
 import { GoogleIcon } from "./icons/GoogleIcon"
 import { GithubIcon } from "./icons/GithubIcon"
+import { Button } from "./Button"
+import { Button as NextButton } from "@nextui-org/react"
 
 export const LoginButtonGroup = () => {
 
   return (
+    <>
     <div className="flex flex-col space-y-3">
       {/* <button onClick={() => signIn('google', { callbackUrl: "http://localhost:3000/dashboard"})}>Google</button>
       <button onClick={() => signIn('github', { callbackUrl: "http://localhost:3000/dashboard"})}>Github</button> */}
-      <Button onClick={() => signIn('google', { callbackUrl: "http://localhost:3000/dashboard"})} color="default" variant="bordered" radius="full" startContent={<GoogleIcon />}>Login with Google</Button>
-      <Button onClick={() => signIn('github', { callbackUrl: "http://localhost:3000/dashboard"})} color="default" variant="bordered" radius="full" startContent={<GithubIcon width="1.4rem" height="1.4rem" />}>Login With Github</Button>
-      <Button>
+      {/* <Button onClick={() => signIn('google', { callbackUrl: "http://localhost:3000/dashboard"})} color="default" variant="bordered" radius="full" startContent={<GoogleIcon />}>Login with Google</Button>
+      <Button onClick={() => signIn('github', { callbackUrl: "http://localhost:3000/dashboard"})} color="default" variant="bordered" radius="full" startContent={<GithubIcon width="1.4rem" height="1.4rem" />}>Login With Github</Button> */}
+      <NextButton>
         <span className="inline-flex items-center">
           <GoogleIcon />
         </span>
         Login with Google
+      </NextButton>
+      <Button startContent={<GoogleIcon />} endContent={<GoogleIcon />}>
+        Button
       </Button>
+      <Button>ボタン</Button>
     </div>
+    <div>
+      <Button>aa</Button>
+      <Button>aa</Button>
+    </div>
+    </>
   )
 }
